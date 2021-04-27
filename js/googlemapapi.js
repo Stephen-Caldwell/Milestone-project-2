c5d37991fd27f25db713ff5ad7803c2d7bccbed7
Stephen-Caldwell <stephencaldwell137@gmail.com>
2021-04-27 19:57:40 +0000

    place api with search box and pagination

--- js/googlemapapi.js
+++ js/googlemapapi.js
@@ -4,10 +4,10 @@
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-function initAutocomplete() {
+function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 53.14, lng: -7.6921 },
-    zoom: 6.75,
+    center: { lat: -33.8688, lng: 151.2195 },
+    zoom: 13,
     mapTypeId: "roadmap",
   });
   // Create the search box and link it to the UI element.
@@ -65,4 +65,62 @@ function initAutocomplete() {
     });
     map.fitBounds(bounds);
   });
+
+  // Create the places service.
+  const service = new google.maps.places.PlacesService(map);
+  let getNextPage;
+  const moreButton = document.getElementById("more");
+
+  moreButton.onclick = function () {
+    moreButton.disabled = true;
+
+    if (getNextPage) {
+      getNextPage();
+    }
+  };
+  // Perform a nearby search.
+  service.nearbySearch(
+    { location: input, radius: 5000, type: "store, points_of_interest" },
+    (results, status, pagination) => {
+      if (status !== "OK" || !results) return;
+      addPlaces(results, map);
+      moreButton.disabled = !pagination || !pagination.hasNextPage;
+
+      if (pagination && pagination.hasNextPage) {
+        getNextPage = () => {
+          // Note: nextPage will call the same handler function as the initial call
+          pagination.nextPage();
+        };
+      }
+    }
+  );
+
+}
+
+function addPlaces(places, map) {
+  const placesList = document.getElementById("places");
+
+  for (const place of places) {
+    if (place.geometry && place.geometry.location) {
+      const image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+      new google.maps.Marker({
+        map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location,
+      });
+      const li = document.createElement("li");
+      li.textContent = place.name;
+      placesList.appendChild(li);
+      li.addEventListener("click", () => {
+        map.setCenter(place.geometry.location);
+      });
+    }
+  }
 }
